a8895a3cc3e3077b82c3a6a3a329d989c05436d0
Lesha Kai <kojjmin@gmail.com>
2023-03-03 16:51:15 +0300

    DB refactoring

--- bookshop_project/book_app/models.py
+++ bookshop_project/book_app/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
 class Book(models.Model):
 
     title = models.CharField(max_length=70)
+    year = models.IntegerField(null=True)
     rating = models.IntegerField()
+    is_best_selling = models.BooleanField(default=False)
+    author = models.CharField(max_length=100, null=True)
+
+    def __str__(self) -> str:
+        return str(self.__dict__)
