95e6d5c7fc282cc116b7b05faae1a7e1279c3ec2
Lesha Kai <kojjmin@gmail.com>
2023-03-21 21:30:36 +0300

    tried using aggregation function Count

--- bookshop_project/book_app/views.py
+++ bookshop_project/book_app/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from . import models
-
+from django.db.models import Count
 
 def get_books_mainpage(request):
 
     books = models.Book.objects.all()
 
+    books_count = books.aggregate(Count('id'))
+
     context_data = {
-        "books": books
+        "books": books,
+        "books_count": books_count
     }
 
     return render(request, 'book_app/base.html', context=context_data)
