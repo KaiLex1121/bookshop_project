242d647a90f5c0cda0aa163c698bcd5bd1c90deb
Lesha Kai <kojjmin@gmail.com>
2023-03-23 17:10:31 +0300

    Admin panel customization

--- bookshop_project/book_app/admin.py
+++ bookshop_project/book_app/admin.py
@@ -4,7 +4,7 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
 
-    list_display = ('title',)
+    list_display = ('title', 'rating')
     prepopulated_fields = {
         "slug": ('title',)
     }
