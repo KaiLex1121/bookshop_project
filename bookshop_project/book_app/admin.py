199b14e879203c064faaa242d961dc5c79801f34
Lesha Kai <kojjmin@gmail.com>
2023-03-24 16:52:57 +0300

    Admin panel customization

--- bookshop_project/book_app/admin.py
+++ bookshop_project/book_app/admin.py
@@ -4,10 +4,20 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
 
-    list_display = ('title', 'rating')
+    list_display = ('title', 'rating', 'status_by_rating', 'book_type')
+    ordering = ('-title', 'rating')
+
     prepopulated_fields = {
         "slug": ('title',)
     }
+
+    @admin.display(description='Book Status')
+    def status_by_rating(self, book: Book) -> str:
+        if book.rating < 50:
+            return 'Shit'
+        if book.rating < 75:
+            return 'So-so'
+        return 'Good'
 
 
 admin.site.register(Book, BookAdmin)
--- bookshop_project/book_app/models.py
+++ bookshop_project/book_app/models.py
@@ -1,8 +1,15 @@
 from django.db import models
-from django.utils.text import slugify
 
 
 class Book(models.Model):
+
+    PROSE = 'Prose'
+    POETRY = 'Poetry'
+
+    BOOK_TYPES_CHOICES = (
+        (PROSE, 'Проза'),
+        (POETRY, 'Поэзия')
+    )
 
     title = models.CharField(max_length=70)
     year = models.IntegerField(null=True)
@@ -10,6 +17,7 @@ class Book(models.Model):
     is_best_selling = models.BooleanField(default=False)
     author = models.CharField(max_length=100, null=True)
     slug = models.SlugField(default="", null=False, db_index=True)
+    book_type = models.CharField(null=True, max_length=6, choices=BOOK_TYPES_CHOICES)
 
     def __str__(self) -> str:
         return str(self.__dict__)
